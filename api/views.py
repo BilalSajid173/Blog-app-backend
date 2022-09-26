427247275205f3357193b7eaab0ff68942b5509d
Bilal <bsajid173@gmail.com>
2022-09-26 15:31:48 +0530

    get data from models

--- api/views.py
+++ api/views.py
@@ -1,24 +1,35 @@
 import json
 from django.http import JsonResponse
-
+from products.models import Product
 # Create your views here.
 
 
 def index(request):
     # request -> httpreq
     # request.body
-    body = request.body  # byte string of JSON data
+    # body = request.body  # byte string of JSON data
+    # data = {}
+    # try:
+    #     data = json.loads(body)  # string of json data -> dictionary
+    # except:
+    #     pass
+    # print(data)
+    # # can also add to the data
+    # # without dict(), request.headers is not json serializable
+    # data['headers'] = dict(request.headers)
+    # data['content_type'] = request.content_type
+    # # handling query params
+    # print(request.GET)  # gets query params
+    # data['params'] = dict(request.GET)
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)  # string of json data -> dictionary
-    except:
-        pass
-    print(data)
-    # can also add to the data
-    # without dict(), request.headers is not json serializable
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-    # handling query params
-    print(request.GET)  # gets query params
-    data['params'] = dict(request.GET)
-    return JsonResponse({"message": "Hi There!!"})
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+        # process of serialization
+        # model instance (model_data)
+        # turn it into a python dict
+        # return json to client
+    return JsonResponse(data)
